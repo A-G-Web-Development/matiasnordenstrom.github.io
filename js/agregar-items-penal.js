3fbdbb635daac0134bf7e340aa9e327c249f1d4b
guidodf98 <guido.di.fiore@gmail.com>
2023-06-13 15:00:24 -0300

    dark mode avances

--- js/agregar-items-penal.js
+++ js/agregar-items-penal.js
@@ -6,28 +6,28 @@ boton.addEventListener("click", function () {
   botonUl.remove();
 
   lista.innerHTML += `
-    <li class="list-group-item text-bg-dark nuevo" style="background-color: #f4f4f4">
+    <li class="list-group-item fondo-dark text-white nuevo">
       <i class="bi bi-chevron-double-right pe-2 text-gold"></i>Audiencias indagatorias y explicativas
     </li>
-    <li class="list-group-item text-bg-dark nuevo" style="background-color: #f4f4f4">
+    <li class="list-group-item fondo-dark text-white nuevo">
       <i class="bi bi-chevron-double-right pe-2 text-gold"></i>Excarcelación y eximision de prision
     </li>
-    <li class="list-group-item text-bg-dark nuevo" style="background-color: #f4f4f4">
+    <li class="list-group-item fondo-dark text-white nuevo">
       <i class="bi bi-chevron-double-right pe-2 text-gold"></i>Elaboración de estrategias de defensas
     </li>
-    <li class="list-group-item text-bg-dark nuevo" style="background-color: #f4f4f4">
+    <li class="list-group-item fondo-dark text-white nuevo">
       <i class="bi bi-chevron-double-right pe-2 text-gold"></i>Juicios orales y abreviados
     </li>
-    <li class="list-group-item text-bg-dark nuevo" style="background-color: #f4f4f4">
+    <li class="list-group-item fondo-dark text-white nuevo">
       <i class="bi bi-chevron-double-right pe-2 text-gold"></i>Salidas transitorias y libertad condicional
     </li>
-    <li class="list-group-item text-bg-dark nuevo" style="background-color: #f4f4f4">
+    <li class="list-group-item fondo-dark text-white nuevo">
       <i class="bi bi-chevron-double-right pe-2 text-gold"></i>Probation (suspensión del juicio prueba)
     </li>
-    <li class="list-group-item text-bg-dark nuevo" style="background-color: #f4f4f4">
+    <li class="list-group-item fondo-dark text-white nuevo">
       <i class="bi bi-chevron-double-right pe-2 text-gold"></i>Denuncias y Querellas
     </li>
-    <li class="list-group-item text-bg-dark nuevo" style="background-color: #f4f4f4">
+    <li class="list-group-item fondo-dark text-white nuevo">
       <span id="boton-cerrar" class="clickeable">Cerrar</span>
     </li>
   `;
