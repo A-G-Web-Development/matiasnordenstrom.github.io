d66e9836df8fcb24f3157782074756ced166a5f6
guidodf98 <guido.di.fiore@gmail.com>
2025-03-26 14:02:05 -0300

    posible fix del envio de email

--- js/handleSubmit.js
+++ js/handleSubmit.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((data) => {
           if (data.success && data.score > 0.5) {
             // reCAPTCHA verificado exitosamente, enviar formulario
-            document.getElementById("formulario").submit();
+            enviarCorreo();
           } else {
             // Mostrar error de reCAPTCHA
             Swal.fire({
@@ -52,57 +52,59 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Obtener los valores de los campos del formulario
-    const formData = new FormData(form);
-    const name = formData.get("nombre");
-    const email = formData.get("email");
-    const message = formData.get("mensaje");
-    const telefono = formData.get("telefono");
+    function enviarCorreo() {
+      // Obtener los valores de los campos del formulario
+      const formData = new FormData(form);
+      const name = formData.get("nombre");
+      const email = formData.get("email");
+      const message = formData.get("mensaje");
+      const telefono = formData.get("telefono");
 
-    // Crear un objeto con los datos del formulario
-    const data = {
-      to: "webdevelopment.ag@gmail.com", // Reemplaza con la dirección de correo electrónico del destinatario
-      titulo: "Nuevo mensaje de contacto",
-      texto: message + "\n Teléfono: " + telefono,
-      subject: "Nuevo mensaje de contacto",
-      fromEmail: email,
-      fromNombre: name,
-    };
+      // Crear un objeto con los datos del formulario
+      const data = {
+        to: "webdevelopment.ag@gmail.com", // Reemplaza con la dirección de correo electrónico del destinatario
+        titulo: "Nuevo mensaje de contacto",
+        texto: message + "\n Teléfono: " + telefono,
+        subject: "Nuevo mensaje de contacto",
+        fromEmail: email,
+        fromNombre: name,
+      };
 
-    // Hacer una solicitud POST a tu endpoint con los datos del formulario
-    fetch("https://api.mpconfort.com.ar/api/send-email-externo", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        console.log(response);
-        if (!response.ok) {
-          throw new Error("Hubo un problema al enviar el correo.");
-        } else {
+      // Hacer una solicitud POST a tu endpoint con los datos del formulario
+      fetch("https://api.mpconfort.com.ar/api/send-email-externo", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          console.log(response);
+          if (!response.ok) {
+            throw new Error("Hubo un problema al enviar el correo.");
+          } else {
+            Swal.fire({
+              icon: "success",
+              title: "Éxito",
+              text: "El correo se envió correctamente.",
+              showConfirmButton: false,
+              timer: 3000, // La alerta se cerrará automáticamente después de 3 segundos
+            });
+          }
+        })
+        .catch((error) => {
           Swal.fire({
-            icon: "success",
-            title: "Éxito",
-            text: "El correo se envió correctamente.",
+            icon: "error",
+            title: "Error",
+            text: "Hubo un error al enviar el correo.",
             showConfirmButton: false,
             timer: 3000, // La alerta se cerrará automáticamente después de 3 segundos
           });
-        }
-      })
-      .catch((error) => {
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          text: "Hubo un error al enviar el correo.",
-          showConfirmButton: false,
-          timer: 3000, // La alerta se cerrará automáticamente después de 3 segundos
         });
-      });
 
-    // Resetear el formulario después de procesar los datos
-    form.reset();
+      // Resetear el formulario después de procesar los datos
+      form.reset();
+    }
   });
 });
